73a7fe587dd828f48ff46d8ce6480207f8fd18f3
Ben <benjamin.johnston1998@gmail.com>
2020-12-09 17:30:34 -0500

    Began implementing stock service, had to create another model for both the search result and the list result (kinda annoying, but it is what it is.)

--- Angular/FinanceSite/src/app/services/stocks.service.ts
+++ Angular/FinanceSite/src/app/services/stocks.service.ts
@@ -1,9 +1,53 @@
+import {  StockSearch } from './../models/stock-search.model';
+import { IEC_API_URL , IEC_API_KEY} from './../../environments/environment.prod';
+import { Stock } from './../models/stock.model';
+import { StockAll } from './../models/stock-all.model';
 import { Injectable } from '@angular/core';
+import {HttpClient,HttpErrorResponse} from '@angular/common/http';
+import { Observable, of, throwError } from 'rxjs';
+import{catchError} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class StocksService {
 
-  constructor() { }
+stockAllRef:Observable<StockAll[]>
+
+  constructor(private http: HttpClient) { }
+
+  //Returns a list of the top stocks with everything but their monetary value. A user will have to search a specific stock to see that (or click on that stock)
+  public getTopStocks():Observable<StockAll[]>
+  {
+    if(this.stockAllRef === null)
+    {
+      this.stockAllRef =  this.http.get<StockAll[]>(`${IEC_API_URL}ref-data/symbols?token=${IEC_API_KEY}`).pipe(
+        catchError(this.handleError<StockAll[]>('getTopStocks',[]))  
+      )
+    }
+    return this.stockAllRef;
+  }
+
+  //Returns a stock based on the symbol. CANNOT DO COMPANY NAME
+  public findStock(symbol:string): Observable<StockSearch>
+  {
+    return this.http.get<StockSearch>(`${IEC_API_URL}stock/${symbol}/quote?token=${IEC_API_KEY}`).pipe(
+      catchError(this.handleError<StockSearch>('findStock',null))  
+    )
+
+  
+  }
+
+  
+
+
+  private handleError<T>(operation = 'operation', result?:T)
+  {
+    return(error: any): Observable<T> =>{
+      console.error(error);
+
+      return of(result as T);
+    }
+  }
+
 }
