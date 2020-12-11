495e8d9132d392e7b6fc864a3786051be18d4b1a
Thomas Morgan <tmorgan1@tamu.edu>
2020-12-11 15:49:22 -0600

    hbm2ddl set to update, usercontroller mapping fixed

--- Java/src/main/java/com/revature/rest/UserController.java
+++ Java/src/main/java/com/revature/rest/UserController.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -44,7 +45,7 @@ public class UserController {
 		return userService.getUserByUserame(user.getUsername());
 	}
 	
-	@PostMapping("/getUsers")
+	@GetMapping("/getUsers")
 	public @ResponseBody List<User> getAllUsers() {
 		return userService.getAllUsers();
 	}
