c68d28d449cacd1c45935b1ddbe9b148a61decd1
Ben <benjamin.johnston1998@gmail.com>
2020-12-17 15:52:11 -0500

    Updated Java build to (hopefully) fix CORS issue

--- Java/src/main/java/com/revature/rest/StockController.java
+++ Java/src/main/java/com/revature/rest/StockController.java
@@ -14,7 +14,7 @@ import com.revature.model.Stock;
 import com.revature.service.StockService;
 
 @Controller("stockController")
-@CrossOrigin(origins = "http://localhost:80")
+@CrossOrigin(origins = "http://100.24.59.129:80")
 public class StockController {
 
 	@Autowired
