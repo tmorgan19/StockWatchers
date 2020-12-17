49a54efbf908253695c640452d1ed69904fa7773
Ben <benjamin.johnston1998@gmail.com>
2020-12-17 17:19:24 -0500

    Final attempt to host project on EC2

--- Angular/FinanceSite/src/environments/environment.prod.ts
+++ Angular/FinanceSite/src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true
 };
 
-export const SERVER_URL = `http://localhost:8080/StockWatchers`
+export const SERVER_URL = `http://100.24.59.129:8080/StockWatchers`
 
 export const IEC_API_KEY = `pk_23cb78326562484ab5a05d73dc740c55`
 export const IEC_API_URL = `https://cloud.iexapis.com/stable/`
