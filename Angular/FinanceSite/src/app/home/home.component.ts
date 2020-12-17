a719e4f5e0a7371249e79a9e1ea45e72ca278127
Young Ha Shin <YHS@Youngs-MBP.verizon.net>
2020-12-17 08:14:19 -0500

    home averge price pipe

--- Angular/FinanceSite/src/app/home/home.component.ts
+++ Angular/FinanceSite/src/app/home/home.component.ts
@@ -48,8 +48,7 @@ export class HomeComponent implements OnInit {
 
     for(let x=0;x<purchases.length;x++){
       if(x==0){
-        let num = purchases[0].price*purchases[0].amount
-        purchases[0].price = parseInt((Math.round(num*100)/100).toFixed(2))
+        purchases[0].price = purchases[0].price*purchases[0].amount
         setPurchases.push(purchases[0]);
         continue;
       }
